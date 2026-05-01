7da5679d7fa53171d250c81bd5df8178ade5b37e
Nazar Vinnichuk <nv@respeecher.com>
2026-05-01 17:43:26 +0300

    Make the initial switch to pipecat 1.1.0

--- example-ukrainian.py
+++ example-ukrainian.py
@@ -49,7 +49,6 @@ from pipecat.processors.aggregators.llm_response_universal import (
     LLMContextAggregatorPair,
     LLMUserAggregatorParams,
 )
-from pipecat.processors.frameworks.rtvi import RTVIConfig, RTVIObserver, RTVIProcessor
 from pipecat.runner.types import RunnerArguments
 from pipecat.runner.utils import create_transport
 from pipecat.transcriptions.language import Language
@@ -80,12 +79,12 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
 
     tts = RespeecherTTSService(
         api_key=os.getenv("RESPEECHER_API_KEY"),
-        voice_id="olesia-conversation",
-        model="public/tts/ua-rt",
-        # [Optional] Sampling parameters overrides.
-        # Can be changed on the fly with TTSUpdateSettingsFrame,
-        # just like the model and the voice.
-        params=RespeecherTTSService.InputParams(
+        settings=RespeecherTTSService.Settings(
+            voice="olesia-conversation",
+            model="public/tts/ua-rt",
+            # [Optional] Sampling parameters overrides.
+            # Can be changed on the fly with TTSUpdateSettingsFrame,
+            # just like the model and the voice.
             sampling_params={
                 "min_p": 0.01,
             },
@@ -123,13 +122,9 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
         ),
     )
 
-    # [Optional] Without RTVI, the chat interface in the WebRTC demo page won't work.
-    rtvi = RTVIProcessor(config=RTVIConfig(config=[]))
-
     pipeline = Pipeline(
         [
             transport.input(),  # Transport user input
-            rtvi,
             stt,
             context_aggregator.user(),  # User responses
             llm,  # LLM
@@ -151,7 +146,7 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
             audio_out_sample_rate=22050,
         ),
         idle_timeout_secs=runner_args.pipeline_idle_timeout_secs,
-        observers=[RTVIObserver(rtvi), whisker],
+        observers=[whisker],
     )
 
     @transport.event_handler("on_client_connected")
